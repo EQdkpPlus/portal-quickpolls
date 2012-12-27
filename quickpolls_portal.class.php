9fc13f6b3064493eeb04228a361e8c0689d85fcf
godmod <godmod@eqdkp-plus.eu>
2012-12-27 12:37:23 +0000

    added Drop table before creating it

--- quickpolls_portal.class.php
+++ quickpolls_portal.class.php
@@ -79,12 +79,14 @@ class quickpolls_portal extends portal_generic {
 	protected $multiple = true;
 	
 	protected $sqls		= array(
+		"DROP TABLE IF EXISTS __quickpolls_votes;",
 		"CREATE TABLE `__quickpolls_votes` (
 		  `poll_id` int(10) unsigned NOT NULL default '0',
 		  `user_id` int(10) unsigned NOT NULL default '0',
 		  KEY `poll_id` (`poll_id`),
 		  KEY `user_id` (`user_id`)
 		) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;",
+		"DROP TABLE IF EXISTS __quickpolls;",
 		"CREATE TABLE `__quickpolls` (
 		  `id` int(10) unsigned NOT NULL auto_increment,
 		  `tstamp` int(10) unsigned NOT NULL default '0',
