35490af61f6fdb3b6eef54ef0ac21464fcf96df7
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

--- language/english.php
+++ language/english.php
@@ -1,6 +1,6 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	Quick polls Portal Module
+ *	Package:	EQdkp-Plus Language File
  *	Link:		http://eqdkp-plus.eu
  *
  *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
@@ -19,25 +19,33 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if ( !defined('EQDKP_INC') ){
-	header('HTTP/1.0 404 Not Found');exit;
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
 }
 
-$lang = array(
-	'quickpolls'					=> 'Quickpoll',
-	'quickpolls_name'				=> 'Quickpoll',
-	'quickpolls_desc'				=> 'Create a poll',
-	'quickpolls_f_title'			=> 'Title of the poll',
-	'quickpolls_f_question'			=> 'Description',
-	'quickpolls_f_help_question'	=> 'Description or Question for the poll, shown under the title',
-	'quickpolls_f_closedate'		=> 'Show until',
-	'quickpolls_f_closedate_help'	=> 'After this date the results will be shown and no more votes are possible',
-	'quickpolls_f_showresults'		=> 'Show Results-Link',
-	'quickpolls_f_help_showresults'	=> 'Results-Link will be shown, independet from voting-status',
-	'quickpolls_f_options'			=> 'Options',
-	'quickpolls_f_help_options'		=> 'Insert one Option per row',
-	'quickpolls_f_resetvotes' 		=> 'Reset votes',
-	'quickpolls_vote'				=> 'Vote',
-	'quickpolls_resuls'				=> 'Results',
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: portal/quickpolls/language/english.php
+//Source-Language: german
+
+$lang = array( 
+	"quickpolls" => 'Quickpoll',
+	"quickpolls_name" => 'Quickpoll',
+	"quickpolls_desc" => 'Create a poll',
+	"quickpolls_f_title" => 'Title of the poll',
+	"quickpolls_f_question" => 'Description',
+	"quickpolls_f_question_help" => 'Description or Question for this Quickpoll, will be shown under the title',
+	"quickpolls_f_closedate" => 'Show until',
+	"quickpolls_f_help_closedate" => 'After this date, only the results will be shown and votings are disabled.',
+	"quickpolls_f_showresults" => 'Show Results-Link',
+	"quickpolls_f_help_showresults" => 'Results-Link will be shown, independet from voting-status',
+	"quickpolls_f_options" => 'Options',
+	"quickpolls_f_help_options" => 'Insert one Option per row',
+	"quickpolls_f_resetvotes" => 'Reset votes',
+	"quickpolls_vote" => 'Vote',
+	"quickpolls_resuls" => 'Results',
+	
 );
+
 ?>
